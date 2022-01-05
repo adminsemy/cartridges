092c411e15a4e84dcf75d2b27d90c95f314c1f0b
Antonov Denis <den_ru1982@mail.ru>
2022-01-05 18:24:14 +0300

    Добавил несколько шаблонов страниц для проекта

---
+++ src/pages/CartridgesCount.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CartridgesCount = () => {
+    return (
+        <div>
+            <h1>Hello CartridgesCount!</h1>
+        </div>
+    )
+}
---
+++ src/pages/CartridgesOrder.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CartridgesOrder = () => {
+    return (
+        <div>
+            <h1>Hello CartridgesOrder!</h1>
+        </div>
+    )
+}
